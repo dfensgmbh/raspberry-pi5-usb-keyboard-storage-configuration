0b533e2bb8866ab240d14d8e7a20d50571e560b8
Ronald Rink <dfch@users.noreply.github.com>
2026-05-17 12:10:50 +0200

    minor, doc

--- root/home/admin/scripts/05_examine_configuration.sh
+++ root/home/admin/scripts/05_examine_configuration.sh
@@ -1,3 +1,3 @@
-ls /dev/hidg*        # Should show /dev/hidg0
-ls /sys/class/udc/   # Should show the UDC controller
-dmesg | grep dwc2    # Check for errors
+ls /dev/hidg*        # Must show /dev/hidg0.
+ls /sys/class/udc/   # Must show the UDC controller.
+dmesg | grep dwc2    # Examine for any errors.
